d2c0a743fcccc9d6548f66330bc69f8b249d969e
Kayden200 <159207295+Kayden200@users.noreply.github.com>
2025-04-15 13:42:51 +0800

    Create admin.js

---
+++ backend/routes/admin.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../models/User');
+const Transaction = require('../models/Transaction');
+const router = express.Router();
+
+// Get all users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// Get all transactions
+router.get('/transactions', async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    res.json(transactions);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
